bc07310e7e6ef937ee017dca550005ba066024d9
YiyiF <yiyifu97@gmail.com>
2021-01-25 11:43:46 +0800

    pytest samples

---
+++ tests/pytestPractice/test_capsys.py
@@ -0,0 +1,21 @@
+import sys
+import pysnooper
+
+
+def ping(output):
+    print('Pong...', file=output)
+
+
+@pysnooper.snoop()
+def test_stdout(capsys):
+    ping(sys.stdout)
+    out, err = capsys.readouterr()
+    assert out == 'Pong...\n'
+    assert err == ''
+
+
+def test_stderr(capsys):
+    ping(sys.stderr)
+    out, err = capsys.readouterr()
+    assert out == ''
+    assert err == 'Pong...\n'
